db895e032f1ffb6d1342b1b2062527da06cb52e7
renatakov <renatakov1010@gmail.com>
2020-10-14 13:15:37 +0300

    done

--- task-5.js
+++ task-5.js
@@ -3,9 +3,15 @@
 // ========================= 5 ========
 
 
-const inputName = document.getElementById('name-input');
-// let inp;
+// Напиши скрипт который, при наборе текста в инпуте input#name-input
+// (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка 'незнакомец'.
 
-// if (inp === 0){
-//   // console.log();
-// }
+// const mySpan = document.getElementById('name-output');
+// const nInput = document.getElementById('name-input');
+// nInput.addEventListener('input', function(){
+//   if (this.value.length !== 0) {
+//     mySpan.innerText = this.value;
+//   } else {
+//     mySpan.innerText = "незнакомец"
+//  };
+// })
